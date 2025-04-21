188b5d2d2baf59224a0d268d71116d0f42be77ef
mothgharenimish <mothgharenimish0@gmail.com>
2025-04-21 22:01:05 +0530

    Initial Commit

---
+++ AnimatedLoginSignUP-UIKIT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimatedLoginSignUP-UIKIT
+//
+//  Created by Nimish Mothghare on 21/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AnimatedLoginSignUP-UIKITTests/AnimatedLoginSignUP_UIKITTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedLoginSignUP_UIKITTests.swift
+//  AnimatedLoginSignUP-UIKITTests
+//
+//  Created by Nimish Mothghare on 21/04/25.
+//
+
+import Testing
+@testable import AnimatedLoginSignUP_UIKIT
+
+struct AnimatedLoginSignUP_UIKITTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
