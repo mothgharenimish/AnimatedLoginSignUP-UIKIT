ce0719fd27282fc91383a69b27ecb7cc68d56938
mothgharenimish <mothgharenimish0@gmail.com>
2025-04-22 17:22:19 +0530

    Xcode Updated changes

---
+++ AnimatedLoginSignUP-UIKIT/View/SplashVC.swift
@@ -0,0 +1,29 @@
+//
+//  SplashVC.swift
+//  AnimatedLoginSignUP-UIKIT
+//
+//  Created by Nimish Mothghare on 21/04/25.
+//
+
+import UIKit
+
+class SplashVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
