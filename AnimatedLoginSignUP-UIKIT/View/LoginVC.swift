22c5a2921476a4abea911db2cbe807c96ca3e4c3
mothgharenimish <mothgharenimish0@gmail.com>
2025-04-23 11:33:11 +0530

    Xcoded Updated Changes

--- AnimatedLoginSignUP-UIKIT/View/LoginVC.swift
+++ AnimatedLoginSignUP-UIKIT/View/LoginVC.swift
@@ -13,14 +13,49 @@ class LoginVC: UIViewController {
     //MARK: -IBOutlet
     @IBOutlet weak var loginView: AnimatableView!
     @IBOutlet weak var emailtxtField: AnimatableTextField!
+    @IBOutlet weak var passwordtxtField: AnimatableTextField!
+    @IBOutlet weak var loginBtn: AnimatableButton!
+    @IBOutlet weak var accountLbl: AnimatableLabel!
+    @IBOutlet weak var signupBtn: AnimatableButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         loginView.animate(.compound(animations: .init(arrayLiteral: .slideFade(way: .in, direction: .down)), run: .parallel))
-        emailtxtField.animate(.compound(animations: .init(arrayLiteral: .slide(way: .in, direction: .down)), run: .parallel))
+      
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+              print("Code executed after 2 seconds")
+            
+            
+            self.emailtxtField.animate(.compound(animations: .init(arrayLiteral: .slide(way: .in, direction: .left)), run: .parallel))
+            
+            
+            self.passwordtxtField.animate(.compound(animations: .init(arrayLiteral: .slide(way: .in, direction: .left)), run: .parallel))
+            
+            self.loginBtn.animate(.compound(animations: .init(arrayLiteral: .slide(way: .in, direction: .left)), run: .parallel))
+            
+            self.accountLbl.animate(.compound(animations: .init(arrayLiteral: .slide(way: .in, direction: .left)), run: .parallel))
+            
+            self.signupBtn.animate(.compound(animations: .init(arrayLiteral: .slide(way: .in, direction: .left)), run: .parallel))
+
+
+            
+          }
     }
     
-
-   
+    
+    
+    
+    @IBAction func signInaction(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+    @IBAction func signupAction(_ sender: UIButton) {
+        
+        
+    }
+    
 }
